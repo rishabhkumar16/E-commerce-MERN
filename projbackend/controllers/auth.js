6626827696e35658328a47214346ca863fff9f3f
Rishabh Kumar <me.rishabh20@gmail.com>
2020-09-07 17:04:23 +0530

    Sign-in functionality added

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -1,9 +1,11 @@
 const User = require("../models/user.js");
-const {check, validationResult} = require('express-validator')
+const {check, validationResult} = require('express-validator');
+var jwt = require('jsonwebtoken');
+var expressJwt = require('express-jwt');
 
 exports.signup = (req, res)=>{
     
-    const errors = validationResult(req)
+    const errors = validationResult(req);
     
     if(!errors.isEmpty()){
         return res.status(422).json({
@@ -24,6 +26,37 @@ exports.signup = (req, res)=>{
             id: user._id
         });
     })
+};
+
+exports.signin = (req,res)=>{
+    const errors = validationResult(req);
+    const{email, password} = req.body;
+    if(!errors.isEmpty()){
+     return res.status(422).json({
+        error: errors.array()[0].msg
+        });
+    };
+    User.findOne({email}, (err, user)=>{
+        if(err || !user){
+            return res.status(400).json({
+                error: "User email doesn't exists"
+            })
+        }
+
+        if(!user.authenticate(password)){
+            return res.status(401).json({
+                error: "Email and Password do not match"
+            })
+        }
+        //create token
+        const token = jwt.sign({_id: user._id},process.env.SECRET)
+        //put token in cookie
+        res.cookie("token", token, {expire: new Date() + 9999});
+
+        //send response to frontend
+        const {_id,name,email, role}=user;
+        return res.json({token, user:{_id,name,email, role}});
+    });
 };
 
 exports.signout = (req, res)=>{
--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -1,13 +1,20 @@
 var express = require('express');
 var router = express.Router();
 const {check, validationResult} = require('express-validator')
-const {signout, signup} = require("../controllers/auth");
+const {signout, signup, signin} = require("../controllers/auth");
 
 router.post("/signup",[
     check("name", "Name should be atleast 3 char").isLength({min: 3}),
     check("email", "email is required").isEmail(),
     check("password", "password should be atleast 5 char").isLength({min:5})
 ] ,signup);
+
+router.post("/signin",[
+    check("email", "email is required").isEmail(),
+    check("password", "password is required").isLength({min:1})
+] ,signin);
+
+
 router.get("/signout", signout);
 
 module.exports = router;
