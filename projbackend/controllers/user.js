26011d90ff882b8f11cad8c30ee1f7f9453557d8
RISHABH KUMAR <me.rishabh20@gmail.com>
2020-09-14 00:40:23 +0530

    now you can update user

--- projbackend/controllers/user.js
+++ projbackend/controllers/user.js
@@ -1,5 +1,5 @@
 const User = require("../models/user");
-
+//signin
 exports.getUserById = (req, res, next, id) => {
   User.findById(id).exec((err, user) => {
     if (err || !user) {
@@ -11,8 +11,39 @@ exports.getUserById = (req, res, next, id) => {
     next();
   });
 };
-
+//getting user details
 exports.getUser = (req, res) => {
-    //kk    
+    req.profile.salt = undefined;
+    req.profile.encry_password = undefined;
     return res.json(req.profile);
-};
+  };
+
+//updating the user
+exports.updateUser = (req,res) => {
+  User.findByIdAndUpdate(
+    {_id : req.profile._id},
+    {$set: req.body},
+    {new: true, useFindAndModify: false},
+    (err, user) => {
+      if(err || !user){
+        return res.status(400).json({
+          error: "You are not authorized to update this user"
+        })
+      }
+      user.salt = undefined;
+      user.encry_password = undefined;
+      res.json(user)
+    }
+  )
+}
+//optional
+  exports.getAllUsers = (req,res) => {
+    User.find().exec((err, users) => {
+      if(err || !users){
+        return res.status(400).json({
+          error: "No User Found"
+        });
+      }
+      res.json(users);
+    });
+  };
