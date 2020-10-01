39612ae4c5b88ecc64d3a078e097a98125639bef
RISHABH KUMAR <me.rishabh20@gmail.com>
2020-10-01 16:55:50 +0530

    Upading and deleting product added

--- projbackend/controllers/product.js
+++ projbackend/controllers/product.js
@@ -51,7 +51,7 @@ exports.createProduct = (req, res) => {
       product.save((err, product) => {
         if (err) {
           res.status(400).json({
-            error: "Saving tshirt in DB failed"
+            error: "Saving product in DB failed"
           });
         }
         res.json(product);
@@ -70,4 +70,55 @@ exports.photo = (req, res, next) => {
     return res.send(req.product.photo.data);
   }
   next();
-}
+};
+
+exports.deleteProduct = (req, res) => { 
+  let product = req.product;
+  product.remove((err, deletedProduct) => {
+    if(err){
+      return res.status(400).json({
+        error: "Failed to delete the product"
+      });
+    }
+    res.json({
+      message: "deletion was a success",
+      deletedProduct
+    });
+  });
+};
+exports.updateProduct = (req, res) => {
+  let form = new formidable.IncomingForm();
+  form.keepExtensions = true;
+
+  form.parse(req, (err, fields, file) => {
+    if (err) {
+      return res.status(400).json({
+        error: "problem with image"
+      });
+    }
+    //updation code
+    let product = req.product;
+    product = _.extend(product, fields)
+    
+    //handle file here
+    if (file.photo) {
+      if (file.photo.size > 3000000) {
+        return res.status(400).json({
+          error: "File size too big!"
+        });
+      }
+      product.photo.data = fs.readFileSync(file.photo.path);
+      product.photo.contentType = file.photo.type;
+    }
+
+    //save to the DB
+    product.save((err, product) => {
+      if (err) {
+        res.status(400).json({
+          error: "Updating product in DB failed"
+        });
+      }
+      res.json(product);
+    });
+  });
+};
--- projbackend/routes/product.js
+++ projbackend/routes/product.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const {getProductById, createProduct, getProduct, photo} = require("../controllers/product");
+const {getProductById, createProduct, getProduct, photo, deleteProduct, updateProduct} = require("../controllers/product");
 const {isSignedIn, isAuthenticated, isAdmin} = require("../controllers/auth");
 const {getUserById} = require("../controllers/user");
 //params
@@ -9,6 +9,13 @@ router.param("userId", getUserById);
 router.param("productId", getProductById);
 //actual routes
 router.post("/product/create/:userId", isSignedIn, isAuthenticated, isAdmin, createProduct);
+//read routes
 router.get("/product/:productId", getProduct);
 router.get("/product/photo/:productId", photo);
+//delete routes
+router.delete("/product/:productId/:userId", isSignedIn, isAuthenticated, isAdmin, deleteProduct);
+//update routes
+router.put("/product/:productId/:userId", isSignedIn, isAuthenticated, isAdmin, updateProduct);
+//listing routes
+
 module.exports = router;
