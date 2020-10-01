fd484636d103663e4f456e56a05e9b6aa4790e35
RISHABH KUMAR <me.rishabh20@gmail.com>
2020-10-01 17:22:18 +0530

    getting the product

--- projbackend/controllers/product.js
+++ projbackend/controllers/product.js
@@ -2,6 +2,7 @@ const Product = require("../models/product");
 const formidable = require("formidable");
 const _ = require("lodash");
 const fs = require("fs");
+const { sortBy } = require("lodash");
 
 exports.getProductById = (req, res, next, id) => {
     Product.findById(id).populate("category").exec((err, product) => {
@@ -120,5 +121,17 @@ exports.updateProduct = (req, res) => {
       }
       res.json(product);
     });
+  });
+};
+exports.getAllProducts = (req, res) => {
+  let limit = req.query.limit ? parseInt(req.query.limit) : 8;
+  let sortBy = req.query.sortBy ? req.query.sortBy : "_id";
+  Product.find().select("-photo").populate("category").sort([[sortBy, "asc"]]).limit(limit).exec((err, products) => {
+    if (err){
+      return res.status(400).json({
+        error: "No product found"
+      });
+    }
+    res.json(products);
   });
 };
--- projbackend/routes/product.js
+++ projbackend/routes/product.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const {getProductById, createProduct, getProduct, photo, deleteProduct, updateProduct} = require("../controllers/product");
+const {getProductById, createProduct, getProduct, photo, deleteProduct, updateProduct, getAllProducts} = require("../controllers/product");
 const {isSignedIn, isAuthenticated, isAdmin} = require("../controllers/auth");
 const {getUserById} = require("../controllers/user");
 //params
@@ -17,5 +17,5 @@ router.delete("/product/:productId/:userId", isSignedIn, isAuthenticated, isAdmi
 //update routes
 router.put("/product/:productId/:userId", isSignedIn, isAuthenticated, isAdmin, updateProduct);
 //listing routes
-
+router.get("/products", getAllProducts);
 module.exports = router;
