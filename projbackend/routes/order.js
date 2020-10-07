41470237b7a4e61d460fec91daa40af565615f2f
RISHABH KUMAR <me.rishabh20@gmail.com>
2020-10-08 00:00:08 +0530

    Enhanced order functionality

--- projbackend/routes/order.js
+++ projbackend/routes/order.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const {isSignedIn, isAuthenticated, isAdmin} = require("../controllers/auth");
 const {getUserById, pushOrderInPurchaseList} = require("../controllers/user"); 
 const {updateStock} = require("../controllers/product");
-const {getOrderById} = require("../controllers/order");
+const {getOrderById, createOrder} = require("../controllers/order");
 
 //params
 router.param("userId", getUserById);
@@ -12,7 +12,7 @@ router.param("orderId", getOrderById);
 //Actual routes
 
 //create
-
+router.post("/order/create/:userId", isSignedIn, isAuthenticated, pushOrderInPurchaseList, updateStock, createOrder);
 //read
 
 module.exports = router;
