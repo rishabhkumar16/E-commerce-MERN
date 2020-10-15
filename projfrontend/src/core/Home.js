7912261497b94a9dda4b4f4a7aeed35b3e39edf9
RISHABH KUMAR <me.rishabh20@gmail.com>
2020-10-15 14:33:09 +0530

    testing in frontend

--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import { API } from '../backend';
 import "../styles.css";
+import Base from './Base';
 
 export default function Home() {
     console.log("API IS", API);
     return (
-        <div>
-            <h1 className="text-white">Hello front end </h1>
-        </div>
+        <Base title="Home Page">
+            <div className="row">
+                <div className="col-4">
+                    <button className="btn btn-success">TEST</button>
+                </div>
+                <div className="col-4">
+                    <button className="btn btn-success">TEST</button>
+                </div>
+                <div className="col-4">
+                    <button className="btn btn-success">TEST</button>
+                </div>
+            </div>
+        </Base>
         );
 }
