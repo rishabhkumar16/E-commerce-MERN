3eb38b282fad93d6cede080a94d77fefc66e7a48
RISHABH KUMAR <me.rishabh20@gmail.com>
2020-10-15 13:32:20 +0530

    Working on frontend

--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { API } from '../backend';
 import "../styles.css";
+
 export default function Home() {
+    console.log("API IS", API);
     return (
         <div>
-            <h1 className="text-white">Hello front end</h1>
+            <h1 className="text-white">Hello front end </h1>
         </div>
         );
 }
