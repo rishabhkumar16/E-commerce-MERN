09e565ef4a4f44a4a0347c16dabd62e3372b92e0
RISHABH KUMAR <me.rishabh20@gmail.com>
2020-10-18 02:55:47 +0530

    Working on frontend

--- projfrontend/src/user/Signin.js
+++ projfrontend/src/user/Signin.js
@@ -3,9 +3,28 @@ import Base from '../core/Base';
  import {Link} from 'react-router-dom';
 
  const Signin  = () => {
+    const signinForm = () => {
+        return (
+            <div className="row">
+                <div className="col-md-6 offset-sm-3 text-left">
+                    <form>
+                        <div className="form-group">
+                            <label className="text-light">Email</label>
+                            <input className="form-control" type="email"/>
+                        </div>    
+                        <div className="form-group">
+                            <label className="text-light">Password</label>
+                            <input className="form-control" type="password"/>
+                        </div>   
+                        <button className="btn btn-success btn-block">Sign In</button>                  
+                    </form>
+                </div>
+            </div>
+        ) 
+    };
      return (
          <Base title="SignIn Page" description="A page for user to signin!!!">
-         <h1>Sign In Works</h1>
+             {signinForm()}
          </Base>
      );
  };
--- projfrontend/src/user/Signup.js
+++ projfrontend/src/user/Signup.js
@@ -10,22 +10,22 @@ import Base from '../core/Base';
                      <form>
                          <div className="form-group">
                              <label className="text-light">Name</label>
-                             <input type="text"/>
+                             <input  className="form-control" type="text"/>
                          </div>
                          <div className="form-group">
                              <label className="text-light">Email</label>
-                             <input type="email"/>
+                             <input className="form-control" type="email"/>
                          </div>    
                          <div className="form-group">
                              <label className="text-light">Password</label>
-                             <input type="password"/>
+                             <input className="form-control" type="password"/>
                          </div>   
-                         <button className="btn btn-success btn-block">Submit</button>                  
+                         <button className="btn btn-success btn-block">Sign Up</button>                  
                      </form>
                  </div>
              </div>
          ) 
-     }
+     };
      return (
          <Base title="Signup Page" description="A page for user to signup!!!">
          
