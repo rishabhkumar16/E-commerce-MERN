e0a982186e21d925c734d5a023eadef531172dda
RISHABH KUMAR <me.rishabh20@gmail.com>
2020-10-18 02:51:31 +0530

    Working on frontend

--- projfrontend/src/user/Signup.js
+++ projfrontend/src/user/Signup.js
@@ -3,9 +3,33 @@ import Base from '../core/Base';
  import {Link} from 'react-router-dom';
 
  const Signup  = () => {
+     const signupForm = () => {
+         return (
+             <div className="row">
+                 <div className="col-md-6 offset-sm-3 text-left">
+                     <form>
+                         <div className="form-group">
+                             <label className="text-light">Name</label>
+                             <input type="text"/>
+                         </div>
+                         <div className="form-group">
+                             <label className="text-light">Email</label>
+                             <input type="email"/>
+                         </div>    
+                         <div className="form-group">
+                             <label className="text-light">Password</label>
+                             <input type="password"/>
+                         </div>   
+                         <button className="btn btn-success btn-block">Submit</button>                  
+                     </form>
+                 </div>
+             </div>
+         ) 
+     }
      return (
          <Base title="Signup Page" description="A page for user to signup!!!">
-         <h1>Sign Up Works</h1>
+         
+         {signupForm()}
          </Base>
      );
  };
