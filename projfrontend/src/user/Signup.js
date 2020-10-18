3898049f8fe4273ead02ba1e37f3db23ee07aa78
RISHABH KUMAR <me.rishabh20@gmail.com>
2020-10-18 14:35:15 +0530

    Sign up functionality added

--- projfrontend/src/user/Signup.js
+++ projfrontend/src/user/Signup.js
@@ -1,36 +1,89 @@
- import React, { useState} from 'react';
-import Base from '../core/Base';
- import {Link} from 'react-router-dom';
+import React, { useState } from "react";
+import Base from "../core/Base";
+import { Link } from "react-router-dom";
+import { signup } from "../auth/helper";
 
- const Signup  = () => {
-     const signupForm = () => {
-         return (
-             <div className="row">
-                 <div className="col-md-6 offset-sm-3 text-left">
-                     <form>
-                         <div className="form-group">
-                             <label className="text-light">Name</label>
-                             <input  className="form-control" type="text"/>
-                         </div>
-                         <div className="form-group">
-                             <label className="text-light">Email</label>
-                             <input className="form-control" type="email"/>
-                         </div>    
-                         <div className="form-group">
-                             <label className="text-light">Password</label>
-                             <input className="form-control" type="password"/>
-                         </div>   
-                         <button className="btn btn-success btn-block">Sign Up</button>                  
-                     </form>
-                 </div>
-             </div>
-         ) 
-     };
-     return (
-         <Base title="Signup Page" description="A page for user to signup!!!">
-         
-         {signupForm()}
-         </Base>
-     );
- };
- export default Signup;
+const Signup = () => {
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    password: "",
+    error: "",
+    success: false
+  });
+
+  const { name, email, password, error, success } = values;
+
+  const handleChange = name => event => {
+    setValues({ ...values, error: false, [name]: event.target.value });
+  };
+
+  const onSubmit = event => {
+    event.preventDefault();
+    setValues({ ...values, error: false });
+    signup({ name, email, password })
+      .then(data => {
+        if (data.error) {
+          setValues({ ...values, error: data.error, success: false });
+        } else {
+          setValues({
+            ...values,
+            name: "",
+            email: "",
+            password: "",
+            error: "",
+            success: true
+          });
+        }
+      })
+      .catch(console.log("Error in signup"));
+  };
+
+  const signUpForm = () => {
+    return (
+      <div className="row">
+        <div className="col-md-6 offset-sm-3 text-left">
+          <form>
+            <div className="form-group">
+              <label className="text-light">Name</label>
+              <input
+                className="form-control"
+                onChange={handleChange("name")}
+                type="text"
+              />
+            </div>
+            <div className="form-group">
+              <label className="text-light">Email</label>
+              <input
+                className="form-control"
+                onChange={handleChange("email")}
+                type="email"
+              />
+            </div>
+
+            <div className="form-group">
+              <label className="text-light">Password</label>
+              <input
+                onChange={handleChange("password")}
+                className="form-control"
+                type="password"
+              />
+            </div>
+            <button onClick={onSubmit} className="btn btn-success btn-block">
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <Base title="Sign up page" description="A page for user to sign up!">
+      {signUpForm()}
+      
+    </Base>
+  );
+};
+
+export default Signup;
