f7529b24459d2e94869a7b16402a41dea0ab96be
RISHABH KUMAR <me.rishabh20@gmail.com>
2020-10-23 01:47:45 +0530

    frontend modified

--- projfrontend/src/user/Signin.js
+++ projfrontend/src/user/Signin.js
@@ -1,31 +1,113 @@
-import React, { useState} from 'react';
-import Base from '../core/Base';
- import {Link} from 'react-router-dom';
+import React, { useState } from "react";
+import Base from "../core/Base";
+import { Link, Redirect } from "react-router-dom";
 
- const Signin  = () => {
-    const signinForm = () => {
-        return (
-            <div className="row">
-                <div className="col-md-6 offset-sm-3 text-left">
-                    <form>
-                        <div className="form-group">
-                            <label className="text-light">Email</label>
-                            <input className="form-control" type="email"/>
-                        </div>    
-                        <div className="form-group">
-                            <label className="text-light">Password</label>
-                            <input className="form-control" type="password"/>
-                        </div>   
-                        <button className="btn btn-success btn-block">Sign In</button>                  
-                    </form>
-                </div>
+import { signin, authenticate, isAuthenticated } from "../auth/helper";
+
+const Signin = () => {
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+    error: "",
+    loading: false,
+    didRedirect: false
+  });
+
+  const { email, password, error, loading, didRedirect } = values;
+  const { user } = isAuthenticated();
+
+  const handleChange = name => event => {
+    setValues({ ...values, error: false, [name]: event.target.value });
+  };
+
+  const onSubmit = event => {
+    event.preventDefault();
+    setValues({ ...values, error: false, loading: true });
+    signin({ email, password })
+      .then(data => {
+        if (data.error) {
+          setValues({ ...values, error: data.error, loading: false });
+        } else {
+          authenticate(data, () => {
+            setValues({
+              ...values,
+              didRedirect: true
+            });
+          });
+        }
+      })
+      .catch(console.log("signin request failed"));
+  };
+
+  const successMessage = () => {
+    return (
+      <div className="row">
+        <div className="col-md-6 offset-sm-3 text-left">
+          <div
+            className="alert alert-success"
+            style={{ display: success ? "" : "none" }}
+          >
+            New account was created successfully. Please{" "}
+            <Link to="/signin">Login Here</Link>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  const errorMessage = () => {
+    return (
+      <div className="row">
+        <div className="col-md-6 offset-sm-3 text-left">
+          <div
+            className="alert alert-danger"
+            style={{ display: error ? "" : "none" }}
+          >
+            {error}
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  const signInForm = () => {
+    return (
+      <div className="row">
+        <div className="col-md-6 offset-sm-3 text-left">
+          <form>
+            <div className="form-group">
+              <label className="text-light">Email</label>
+              <input
+                onChange={handleChange("email")}
+                value={email}
+                className="form-control"
+                type="email"
+              />
             </div>
-        ) 
-    };
-     return (
-         <Base title="SignIn Page" description="A page for user to signin!!!">
-             {signinForm()}
-         </Base>
-     );
- };
- export default Signin;
+
+            <div className="form-group">
+              <label className="text-light">Password</label>
+              <input
+                onChange={handleChange("password")}
+                value={password}
+                className="form-control"
+                type="password"
+              />
+            </div>
+            <button onClick={onSubmit} className="btn btn-success btn-block">
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <Base title="Sign In page" description="A page for user to sign in!">
+      {signInForm()}
+    </Base>
+  );
+};
+
+export default Signin;
