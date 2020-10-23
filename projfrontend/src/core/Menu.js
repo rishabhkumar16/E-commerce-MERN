76a93052039ea390a8a81c8e1ec099ec70135695
RISHABH KUMAR <me.rishabh20@gmail.com>
2020-10-24 00:54:08 +0530

    Routes updated

--- projfrontend/src/core/Menu.js
+++ projfrontend/src/core/Menu.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Fragment} from "react";
 import { Link, withRouter } from "react-router-dom";
 import {signout, isAuthenticated} from "../auth/helper";
 
@@ -46,24 +46,28 @@ const Menu = ({ history }) => (
           A. Dashboard
         </Link>
       </li>
-      <li className="nav-item">
-        <Link
-          style={currentTab(history, "/signup")}
-          className="nav-link"
-          to="/signup"
-        >
-          Signup
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          style={currentTab(history, "/signin")}
-          className="nav-link"
-          to="/signin"
-        >
-          Sign In
-        </Link>
-      </li>
+      {!isAuthenticated() && (
+           <Fragment>
+           <li className="nav-item">
+             <Link
+               style={currentTab(history, "/signup")}
+               className="nav-link"
+               to="/signup"
+             >
+               Signup
+             </Link>
+           </li>
+           <li className="nav-item">
+             <Link
+               style={currentTab(history, "/signin")}
+               className="nav-link"
+               to="/signin"
+             >
+               Sign In
+             </Link>
+           </li>
+         </Fragment>
+      )}
       {isAuthenticated() && (
       <li className="nav-item">
         <span className="nav-link text-warning" onClick={() => {
@@ -71,7 +75,7 @@ const Menu = ({ history }) => (
             history.push("/")
           });
         }}>
-          Signout  
+          Sign Out  
         </span>  
       </li>
       )}
