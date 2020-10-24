8d4617756fa81355d91da6f0d8038e0f9d560a1b
RISHABH KUMAR <me.rishabh20@gmail.com>
2020-10-25 01:40:34 +0530

    Admin Panel modified

---
+++ projfrontend/src/admin/AddCategory.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Base from '../core/Base';
+
+const AddCategory = () => {
+    return (
+        <div>
+           <Base title="Create a category here" description="Add a new category" className="container bg-info p-4">
+               <div className="row bg-white rounded">
+                   <div className="col-md-8 offset-md-2">
+                       <h1>Hello</h1>
+                   </div>
+               </div>
+           </Base>
+        </div>
+    )
+}
+
+export default AddCategory;
