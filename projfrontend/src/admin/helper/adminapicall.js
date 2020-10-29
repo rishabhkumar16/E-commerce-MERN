d1dc1c10569ec72a064878a3f4c09638cdf068bd
RISHABH KUMAR <me.rishabh20@gmail.com>
2020-10-29 23:51:11 +0530

    Admin API call updated

--- projfrontend/src/admin/helper/adminapicall.js
+++ projfrontend/src/admin/helper/adminapicall.js
@@ -15,3 +15,72 @@ export const createCategory = (userId, token, category) => {
     })
     .catch(err => console.log(err));
 };
+//get all categories
+export const getCategories = () => {
+  return fetch(`${API}/categories`,{
+    method: "GET"
+  }).then(response => {
+    return response.json();
+  })
+  .catch(err => console.log(err));
+};
+//product calls
+//create products
+export const createaProduct = (userId,token, product) => {
+  return fetch(`${API}/product/create/${userId}`,{
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${token}`
+    },
+    body: product
+  }).then(response => {
+    return response.json();
+  })
+  .catch(err => console.log(err)); 
+};
+//read all products
+export const getProducts = () => {
+  return fetch(`${API}/products`,{
+    method: "GET"
+  }).then(response => {
+    return response.json();
+  })
+  .catch(err => console.log(err));
+};
+//delete a product
+export const deleteProduct = (productId, userId, token) => {
+  return fetch(`${API}/product/${productId}/${userId}`,{
+    method: "DELETE",
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${token}`
+    }
+  }).then(response => {
+    return response.json();
+  })
+  .catch(err => console.log(err)); 
+};
+//get a product
+export const getProduct = productId => {
+  return fetch(`${API}/product/${productId}`,{
+    method: "GET"
+  }).then(response => {
+    return response.json();
+  })
+  .catch(err => console.log(err)); 
+};
+//update a product
+export const updateProduct = (productId, userId, token, product) => {
+  return fetch(`${API}/product/${productId}/${userId}`,{
+    method: "PUT",
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${token}`
+    },
+    body: product
+  }).then(response => {
+    return response.json();
+  })
+  .catch(err => console.log(err)); 
+};
