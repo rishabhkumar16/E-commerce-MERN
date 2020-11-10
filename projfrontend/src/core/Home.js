e03566fd922c5d4e930a34761e8ed1779042c9b9
RISHABH KUMAR <me.rishabh20@gmail.com>
2020-11-10 09:03:54 +0530

    Working on Homepage

--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -2,14 +2,15 @@ import React from 'react';
 import { API } from '../backend';
 import "../styles.css";
 import Base from './Base';
+import Card from './Card';
 
 export default function Home() {
-    console.log("API IS", API);
+    
     return (
         <Base title="Home Page" description="Welcome to E-commerce website">
             <div className="row">
                 <div className="col-4">
-                    <button className="btn btn-success">TEST</button>
+                    <Card></Card>
                 </div>
                 <div className="col-4">
                     <button className="btn btn-success">TEST</button>
