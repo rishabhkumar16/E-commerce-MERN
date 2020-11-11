d900848e0f70480b6efcb859de61e022bfa04b3d
RISHABH KUMAR <me.rishabh20@gmail.com>
2020-11-11 12:57:12 +0530

    Fixing bugs on Card

--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -1,23 +1,43 @@
-import React from 'react';
+import React , {useState, useEffect} from 'react';
 import { API } from '../backend';
 import "../styles.css";
 import Base from './Base';
 import Card from './Card';
+import { getProducts } from './helper/coreapicalls';
 
 export default function Home() {
+
+    const [products, setProducts] = useState([]);
+    const [error, setError] = useState(false);
+
+    const loadAllProduct = () => {
+        getProducts().then(data => {
+            if (data.error){
+                setError(data.error);
+            }else{
+                setProducts(data);
+            }
+        });
+    };
+    useEffect(() => {
+        loadAllProduct()
+    },[])
     
+
     return (
         <Base title="Home Page" description="Welcome to E-commerce website">
             <div className="row ">
-                <div className="col-4">
-                    <Card></Card>
+                <h1 className="text-white mb-5">All Products</h1>
+                <div className="row">
+                    {products.map((product,index) => {
+                        return (
+                            <div key={index} className="col-4 mb-4">
+                                <Card/>
+                            </div>
+                        );
+                    })}
                 </div>
-                <div className="col-4">
-                    <button className="btn btn-success">TEST</button>
-                </div>
-                <div className="col-4">
-                    <button className="btn btn-success">TEST</button>
-                </div>
+                
             </div>
         </Base>
         );
